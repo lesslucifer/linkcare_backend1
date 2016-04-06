7dc8cf422c085156016f40032d971ba499e35799
Luu Quang Vu <vulq@ghn.vn>
2016-04-06 23:42:49 +0700

    + add database

--- trunk/implementation/cmn-libs/clinic-data/src/main/java/com/clinic/clinic/common/utils/JsonJava8TimeSerialization.java
+++ trunk/implementation/cmn-libs/clinic-data/src/main/java/com/clinic/clinic/common/utils/JsonJava8TimeSerialization.java
@@ -1,7 +1,6 @@
 package com.clinic.clinic.common.utils;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
