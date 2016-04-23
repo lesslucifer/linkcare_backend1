c1c951b2865c699ee453b5291c44c7f988a761a4
vuongdt92 <vuongdt92@gmail.com>
2016-04-23 11:54:29 +0700

    message

--- trunk/design/database/01_master_data.sql
+++ trunk/design/database/01_master_data.sql
@@ -1,3 +1,5 @@
+use cliniccore_db;
+
 ALTER TABLE `cliniccore_db`.`subcategory` 
 CHANGE COLUMN `name` `name` TEXT(1000) NULL DEFAULT NULL ;
 
@@ -24,11 +26,20 @@ INSERT INTO `cliniccore_db`.`subcategory` (`name`, `code`, `is_deleted`, `catego
 INSERT INTO `cliniccore_db`.`subcategory` (`name`, `code`, `is_deleted`, `category_id`, `clinic_dur`, `patient_home_dur`) VALUES ('Chăm sóc căng sữa', 'CHAM_SOC_CANG_SUA', '0', '10', '10', '30');
 INSERT INTO `cliniccore_db`.`subcategory` (`name`, `code`, `is_deleted`, `category_id`, `clinic_dur`, `patient_home_dur`) VALUES ('Chăm sóc bệnh theo yêu cầu', 'CHAM_SOC_BENH_THEO_YEU_CAU', '0', '11', '10', '30');
 
-insert INTO `cliniccore_db`.`account` (`annual_fee_id`, `place_id`, `subcategory_id`, `cost_id`, `address_id`, `experience`, `code`, `phone_number`, `id_card`, `first_name`, `last_name`, `midle_name`, `gender`, `login_name`, `email`, `birthday`, `image_url`, `hashed_password`, `active_flag`, `is_deleted`, `birdthday`) VALUES ('1', '1', '5', '1', '1', '2', 'NURSE000001', '0909448445', '215215115', 'Bích', 'Tô', 'Ngọc', '2', '215215115', 'bichto@gmail.com', '12234', '/var/hcmus/abc.png', 'e10adc3949ba59abbe56e057f20f883e', '1', '0', '0');
+INSERT INTO `cliniccore_db`.`account` (`annual_fee_id`, `place_id`, `subcategory_id`, `cost_id`, `address_id`, `experience`, `code`, `phone_number`, `id_card`, `first_name`, `last_name`, `midle_name`, `gender`, `login_name`, `email`, `birthday`, `image_url`, `hashed_password`, `active_flag`, `is_deleted`, `birdthday`) VALUES ('1', '1', '5', '1', '1', '2', 'NURSE000001', '0909448445', '215215115', 'Bích', 'Tô', 'Ngọc', '2', '215215115', 'bichto@gmail.com', '12234', '/var/hcmus/abc.png', 'e10adc3949ba59abbe56e057f20f883e', '1', '0', '0');
 INSERT INTO `cliniccore_db`.`account` (`annual_fee_id`, `place_id`, `subcategory_id`, `cost_id`, `address_id`, `experience`, `code`, `phone_number`, `id_card`, `first_name`, `last_name`, `midle_name`, `gender`, `login_name`, `email`, `birthday`, `image_url`, `hashed_password`, `active_flag`, `is_deleted`, `birdthday`) VALUES ('1', '1', '6', '1', '1', '3', 'NURSE000002', '0933002993', '215215116', 'Nguyên', 'Nguyễn', 'Thảo', '2', '215215116', 'nguyen@gmail.com', '323412', '/var/hcmus/abc.png', 'e10adc3949ba59abbe56e057f20f883e', '1', '0', '0');
 INSERT INTO `cliniccore_db`.`account` (`annual_fee_id`, `place_id`, `subcategory_id`, `cost_id`, `address_id`, `experience`, `code`, `phone_number`, `id_card`, `first_name`, `last_name`, `midle_name`, `gender`, `login_name`, `email`, `birthday`, `image_url`, `hashed_password`, `active_flag`, `is_deleted`, `birdthday`) VALUES ('1', '1', '7', '1', '1', '2', 'NURSE000003', '0989331242', '215215117', 'Ngô', 'Tô', 'Kim', '2', '215215117', 'nguyet@gmail.com', '23123', '/var/hcmus/abc.png', 'e10adc3949ba59abbe56e057f20f883e', '1', '0', '0');
 INSERT INTO `cliniccore_db`.`account` (`annual_fee_id`, `place_id`, `subcategory_id`, `cost_id`, `address_id`, `experience`, `code`, `phone_number`, `id_card`, `first_name`, `last_name`, `midle_name`, `gender`, `login_name`, `email`, `birthday`, `image_url`, `hashed_password`, `active_flag`, `is_deleted`, `birdthday`) VALUES ('1', '1', '8', '1', '1', '4', 'NURSE000004', '0967234455', '215215118', 'Vu', 'Nguyên', 'Văn', '1', '215215118', 'vu@gmail.com', '34121', '/var/hcmus/abc.png', 'e10adc3949ba59abbe56e057f20f883e', '1', '0', '0');
 INSERT INTO `cliniccore_db`.`account` (`annual_fee_id`, `place_id`, `subcategory_id`, `cost_id`, `address_id`, `experience`, `code`, `phone_number`, `id_card`, `first_name`, `last_name`, `midle_name`, `gender`, `login_name`, `email`, `birthday`, `image_url`, `hashed_password`, `active_flag`, `is_deleted`, `birdthday`) VALUES ('1', '1', '9', '1', '1', '4', 'NURSE000005', '0967234455', '215215119', 'Tuan', 'Nguyên', 'Văn', '1', '215215119', 'tuan@gmail.com', '34121', '/var/hcmus/abc.png', 'e10adc3949ba59abbe56e057f20f883e', '1', '0', '0');
 
 INSERT INTO `cliniccore_db`.`role` (`code`, `name`, `description`, `is_deleted`) VALUES ('NURSE_ROLE', 'Nurse\'s role', 'Nurse\'s role', '0');
-INSERT INTO `cliniccore_db`.`role` (`code`, `name`, `description`, `is_deleted`) VALUES ('USER_ROLE', 'User\'s role', 'User\'s role', '0');
+
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('3', '2', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('3', '3', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('3', '4', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('3', '5', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('3', '6', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('2', '2', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('2', '3', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('2', '4', '0');
+INSERT INTO `cliniccore_db`.`account_role` (`role_id`, `account_id`, `is_deleted`) VALUES ('2', '5', '0');
--- trunk/design/database/function_distance.sql
+++ trunk/design/database/function_distance.sql
@@ -3,8 +3,7 @@ DROP function IF EXISTS GeoDistDiff;
 
 DELIMITER $$
 USE cliniccore_db$$
-CREATE DEFINER=cliniccore@localhost FUNCTION GeoDistDiff(type ENUM('mi', 'km'), lat1 DECIMAL(30,15), lon1 DECIMAL(30,15), lat2 DECIMAL(30,15), lon2 DECIMAL(30,15) ) RETURNS decimal(30,15)
+CREATE DEFINER=root@localhost FUNCTION GeoDistDiff(type ENUM('mi', 'km'), lat1 DECIMAL(30,15), lon1 DECIMAL(30,15), lat2 DECIMAL(30,15), lon2 DECIMAL(30,15) ) RETURNS decimal(30,15)
 BEGIN
   RETURN ( IF(type = 'km', 6371, 3959) * acos( cos( radians(lat2) ) * cos( radians( lat1 ) ) * cos( radians( lon1 ) - radians(lon2) ) + sin( radians(lat2) ) * sin( radians( lat1 ) ) ) );
 END$$
-DELIMITER;
